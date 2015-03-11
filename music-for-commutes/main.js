1d440b1bf07f09b401c87616628cde9026765c2f
Seth Kranzler <sethkranzler@gmail.com>
2015-03-10 20:38:27 -0400

    Testing

--- music-for-commutes/main.js
+++ music-for-commutes/main.js
@@ -1,3 +1,14 @@
-var osc = new Tone.Oscillator(440, "square")
-    .toMaster() //connected to the master output
-    .start(); // start it right away
+//create one of Tone's built-in synthesizers
+var synth = new Tone.MonoSynth();
+
+//connect the synth to the master output channel
+synth.toMaster();
+
+//create a callback which is invoked every quarter note
+Tone.Transport.setInterval(function(time){
+    //trigger middle C for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n", time);
+}, "4n");
+
+//start the transport
+Tone.Transport.start();
