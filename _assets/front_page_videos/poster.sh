d315a6586203f9ba2a8e75a875b83aa3d3b99280
Seth Kranzler <sethkranzler@gmail.com>
2017-04-24 09:09:53 -0400

    update script to check for files and also create gif versions

--- _assets/front_page_videos/poster.sh
+++ _assets/front_page_videos/poster.sh
@@ -3,5 +3,10 @@
 FILES=./*.mp4
 for f in $FILES
 do
-  ffmpeg -y -i $f -ss 00:00:00 -vframes 1 "${f%.*}".jpg
+  if [ ! -f "${f%.*}".jpg ]
+  then ffmpeg -y -i $f -ss 00:00:00 -vframes 1 "${f%.*}".jpg
+  fi
+  if [ ! -f "${f%.*}".gif ]
+  then gifify $f -o "${f%.*}".gif --colors 255
+  fi
 done
