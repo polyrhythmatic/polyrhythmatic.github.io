e810c3461075bf52929c2c54af48a9bad371f1d0
Seth Kranzler <sethkranzler@gmail.com>
2017-04-24 09:23:14 -0400

    removing gifs... inline video is supported well enough

--- _assets/front_page_videos/poster.sh
+++ _assets/front_page_videos/poster.sh
@@ -6,7 +6,7 @@ do
   if [ ! -f "${f%.*}".jpg ]
   then ffmpeg -y -i $f -ss 00:00:00 -vframes 1 "${f%.*}".jpg
   fi
-  if [ ! -f "${f%.*}".gif ]
-  then gifify $f -o "${f%.*}".gif --colors 255
-  fi
+  # if [ ! -f "${f%.*}".gif ]
+  # then gifify $f -o "${f%.*}".gif --colors 255
+  # fi
 done
